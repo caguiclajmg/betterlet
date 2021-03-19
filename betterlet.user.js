991b25ab29f5ae2d946ee641593abb5894e39b8a
John Mark Gabriel Caguicla <jmg.caguicla@guarandoo.jp>
2021-03-19 18:00:34 +0800

    Add ad filtering

--- betterlet.user.js
+++ betterlet.user.js
@@ -15,6 +15,12 @@
 
 /* globals GM_config */
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function addSettingsButton(config, parent) {
     const span = document.createElement('span');
     span.className = 'ToggleFlyout';
@@ -27,7 +33,7 @@ function addSettingsButton(config, parent) {
     anchor.className = 'MeButton';
 
     const icon = document.createElement('span');
-    icon.innerText = 'í±€';
+    icon.innerText = 'BetterLET';
     anchor.appendChild(icon);
 
     return span;
@@ -50,6 +56,10 @@ function isCommentFiltered(config, element) {
     return false;
 }
 
+function isAdFiltered(config, element) {
+    return config.style.navigation.hideads;
+}
+
 function filterThread(config, element) {
     switch(config.thread.filter.action) {
         case 'Hide':
@@ -66,6 +76,27 @@ function filterThread(config, element) {
 
 function filterComment(config, element) {
     element.style.display = 'none';
+}
+
+function filterAd(config, element) {
+    const adImageReplacements = [
+        'https://media.tenor.com/images/69835c92b959e460edfc6ad7f6f9e61c/tenor.gif',
+        'https://i.kym-cdn.com/photos/images/newsfeed/001/117/432/515.gif',
+        'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/53220c91-0c3c-4cd9-92e3-0152ff27a86c/dd24v4o-187f0036-d179-40c4-b9a0-fc72d67989d8.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzUzMjIwYzkxLTBjM2MtNGNkOS05MmUzLTAxNTJmZjI3YTg2Y1wvZGQyNHY0by0xODdmMDAzNi1kMTc5LTQwYzQtYjlhMC1mYzcyZDY3OTg5ZDguZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.3IOyaG-veVLGmNpHIyta5s40b0U12HoNASmE4nWEZbM',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS753V3owMmBI4pK125Nej3RNHqMR3bx_9yZw&usqp=CAU',
+        'https://i.redd.it/5kajy4ncbl411.png',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMyBgqrtSYb-98JaYYtuoIiipsCxy9Sc-8Qw&usqp=CAU',
+    ];
+
+    const images = element.querySelectorAll('img');
+    images.forEach(e => {
+        e.src = adImageReplacements[getRandomInt(0, adImageReplacements.length - 1)];
+        e.style.width = '250px';
+        e.style.height = '250px';
+    });
+
+    const anchors = element.querySelectorAll('a');
+    anchors.forEach(e => { e.href = 'https://www.clownsinternational.com/' });
 }
 
 GM_config.init({
@@ -94,6 +125,12 @@ GM_config.init({
             type: 'text',
             default: '',
             section: ['Comment Filtering', '']
+        },
+        'style.navigation.hideads': {
+            label: 'I like to complain about ads',
+            type: 'checkbox',
+            default: false,
+            section: ['Navigation', '']
         },
         'style.externalcss.fat32': {
             label: 'FAT32',
@@ -127,6 +164,9 @@ GM_config.init({
             externalcss: [
                 { id: 'css_fat32', enabled: GM_config.get('style.externalcss.fat32') },
             ],
+            navigation: {
+                hideads: GM_config.get('style.navigation.hideads'),
+            },
         },
     };
 
@@ -140,4 +180,8 @@ GM_config.init({
 
     const comments = [...document.querySelectorAll("li[id^='Comment_']")];
     comments.filter(e => isCommentFiltered(config, e)).forEach(e => filterComment(config, e));
+
+    const ads = [...document.querySelectorAll("div[class='BoxFilter BoxDiscussionFilter'] ~ div:not(.Box):not(.BoxCategories)")];
+    ads.filter(e => isAdFiltered(config, e)).forEach(e => filterAd(config, e));
 })();
+
